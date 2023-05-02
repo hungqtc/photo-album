2ca86581dfd60662314f705a29acfc0baf72f3f1
hungtran <hungqtc97@gmail.com>
2023-05-03 00:03:17 +0700

    edit API config file

--- src/main/java/com/hung/albumphoto/config/OpenAPIConfig.java
+++ src/main/java/com/hung/albumphoto/config/OpenAPIConfig.java
@@ -1,2 +1,46 @@
-package com.hung.albumphoto.config;public class OpenAPIConfig {
+package com.hung.albumphoto.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class OpenAPIConfig {
+    @Value("${spring.openapi.prod-url}")
+    private String prodUrl;
+    @Value("${spring.openapi.local-url}")
+    private String localUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI() {
+        Server localServer = new Server();
+        localServer.setUrl(localUrl);
+        localServer.setDescription("Server URL in Development environment");
+
+        Server prodServer = new Server();
+        prodServer.setUrl(prodUrl);
+        prodServer.setDescription("Server URL in Production environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("hungqtc97@gmail.com");
+        contact.setName("hungtran");
+
+        License mitLicense = new License().name("MIT License").url("https://choosealicense.com/licenses/mit/");
+
+        Info info = new Info()
+                .title("Tutorial Management API")
+                .version("3.0")
+                .contact(contact)
+                .description("This API exposes endpoints to manage tutorials.")
+                .license(mitLicense);
+
+        return new OpenAPI().info(info).servers(List.of(localServer, prodServer));
+    }
 }
